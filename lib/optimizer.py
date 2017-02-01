71e8f2a9b298d6015970658fc3b10710d63e7ff8
yasufumi <yasufumi.taniguchi@gmail.com>
2017-02-01 20:24:20 +0900

    update optimizer.save_opt

--- lib/optimizer.py
+++ lib/optimizer.py
@@ -1,6 +1,13 @@
+import os
+from datetime import datetime
+
 from chainer import serializers
 
-def save_opt(opt, filename):
+def save_opt(opt, filename, suffix=False):
+    if suffix:
+        now = datetime.now().strftime('%Y%m%d')
+        root, ext = os.path.splitext(filename)
+        filename = '{}{}{}'.format(root, now, ext)
     serializers.save_npz(filename, opt)
 
 def load_opt(opt, filename):
