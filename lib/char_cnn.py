d875f77398ab876fd96f795e51a14c12c085ea37
yasufumi <yasufumi.taniguchi@gmail.com>
2017-04-17 23:33:53 +0900

    add a weight initializer in char_cnn.py

--- lib/char_cnn.py
+++ lib/char_cnn.py
@@ -3,19 +3,21 @@ from math import ceil
 import chainer
 import chainer.functions as F
 import chainer.links as L
+from chainer.initializers import Normal
 
 from tools.model import BaseModel
 
 class CharCNN(BaseModel):
     def __init__(self, length, categories):
         fc_size = ceil((length - 96) / 27) * 256
+        initializer = Normal(scale=0.05) # mean is 0, standard deviation is 0.05
         super().__init__(
-                conv1 = L.Convolution2D(1, 256, (1, 7)),
-                conv2 = L.Convolution2D(256, 256, (1, 7)),
-                conv3 = L.Convolution2D(256, 256, (1, 3)),
-                conv4 = L.Convolution2D(256, 256, (1, 3)),
-                conv5 = L.Convolution2D(256, 256, (1, 3)),
-                conv6 = L.Convolution2D(256, 256, (1, 3)),
+                conv1 = L.Convolution2D(1, 256, (1, 7), initialW=initializer),
+                conv2 = L.Convolution2D(256, 256, (1, 7), initialW=initializer),
+                conv3 = L.Convolution2D(256, 256, (1, 3), initialW=initializer),
+                conv4 = L.Convolution2D(256, 256, (1, 3), initialW=initializer),
+                conv5 = L.Convolution2D(256, 256, (1, 3), initialW=initializer),
+                conv6 = L.Convolution2D(256, 256, (1, 3), initialW=initializer),
                 fc1 = L.Linear(fc_size, 1024),
                 fc2 = L.Linear(1024, 1024),
                 fc3 = L.Linear(1024, categories)
